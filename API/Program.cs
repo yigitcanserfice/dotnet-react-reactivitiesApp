7f6183f5ba17662a0f334ac7a28fdc195753efad
yigitcanSerfice <yigit_serfice@hotmail.com>
2025-06-04 13:45:41 +0300

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Run();
